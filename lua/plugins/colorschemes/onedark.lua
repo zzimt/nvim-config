9e32398fc5b0cfb3b978073a127f1aefe68c688a
2IMT <daniil_t04@mail.ru>
2026-03-27 10:52:34 +0300

    Cleanup themes

--- lua/plugins/colorschemes/onedark.lua
+++
@@ -1,8 +0,0 @@
-MiniDeps.add({
-	source = "navarasu/onedark.nvim",
-})
-
-require("onedark").setup({
-	style = "warm",
-})
-require("onedark").load()
--- lua/plugins/colorschemes/tokyonight.lua
+++
@@ -1,7 +0,0 @@
-MiniDeps.add({
-	source = "folke/tokyonight.nvim",
-})
-
-require("tokyonight").setup({})
-
-vim.cmd("colorscheme tokyonight")
