3210d3e451bbb4af23b8d03e6ef5de5adbd3e8ae
2IMT <daniil_t04@mail.ru>
2026-03-29 10:30:17 +0300

    Move diagnostic sign settings to vim.diagnostic.config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,15 +13,16 @@ for _, lsp in ipairs(lsps) do
 	vim.lsp.enable(lsp)
 end
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
 	virtual_text = { true, prefix = "●" },
-	signs = true,
 	underline = true,
 	update_in_insert = false,
 	severity_sort = true,
