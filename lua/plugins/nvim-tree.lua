25f4a9c7a9ce056605e8e9ad2a3baa90a6575865
2IMT <daniil_t04@mail.ru>
2026-04-11 11:00:06 +0300

    Remove code to disable default statusline for nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -27,13 +27,3 @@ require("nvim-tree").setup({
 		},
 	},
 })
-
-local nt_api = require("nvim-tree.api")
-
-nt_api.events.subscribe(nt_api.events.Event.TreeOpen, function()
-	local tree_winid = nt_api.tree.winid()
-
-	if tree_winid ~= nil then
-		vim.api.nvim_set_option_value("statusline", " ", { win = tree_winid })
-	end
-end)
