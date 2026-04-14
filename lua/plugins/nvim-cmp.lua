dc3e02121525f3d8ce80f1811159c016a9e9ca98
2IMT <daniil_t04@mail.ru>
2026-04-14 11:24:06 +0300

    Remap autocomplete confirm to <C-a>

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -37,7 +37,7 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
 		["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+		["<C-a>"] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = {
 		{ name = "nvim_lsp" },
